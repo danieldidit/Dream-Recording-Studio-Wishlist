5c4161ebdf5e558cffd5b3c57c0ae46250d5e0a3
Daniel Humphreys <danielhumphreys@live.ca>
2022-04-06 20:09:51 -0400

    Commit # 1 (Assigment 2) Started off where Assignment 1 ended. I created a header and added it to all the pages. Updated information on all the pages, and added/removed content. Added a 404 page and a .htaccess to deal with 404's. ( Not everything was saved to Github first attempt)

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'We could not find that page';
+require 'includes/header.php';
+?>
+
+<main class="four0four">
+    <div>
+        <h1>Looks like that page is missing..</h1>
+        <p>Please use one of the links above then try again.</p>
+    </div>
+</main>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>My Dream Recording Studio | <?php echo $title; ?></title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
+    <!-- Custom JS -->
+    <script src="js/scripts.js" type="text/javascript"></script>
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">My Dream Recording Studio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="wishlist.php">View WishList</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="wishlist-details.php">Add To WishList</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <?php
+                // access the current session
+                if ( session_status() == PHP_SESSION_NONE) {
+                    session_start();
+                }
+                if (empty($_SESSION['username'])){
+                    echo '<li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>';
+                    }
+                else {
+                    echo '<li class="nav-item">
+                        <a class="nav-link" href="#">' . $_SESSION['username'] . '</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav>
